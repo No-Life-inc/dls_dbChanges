e4ac7d410993333b0d6310dc7b8ea478e471b931
ZackO <zo@pyra.dk>
2024-04-23 11:15:42 +0200

    add comment channel

--- subscriber.py
+++ subscriber.py
@@ -7,40 +7,57 @@ import json
 # Load environment variables from .env file
 load_dotenv()
 
+print(os.getenv('RABBITUSER'))
 # RabbitMQ connection parameters
 RABBITMQ_URL = f"amqp://{os.getenv('RABBITUSER')}:{os.getenv('RABBITPW')}@{os.getenv('RABBITURL')}/%2F"
-RABBITMQ_QUEUE = 'new_stories'
+RABBITMQ_STORIES_QUEUE = 'new_stories'
+RABBITMQ_COMMENTS_QUEUE = 'new_comments'
 
 # MongoDB connection parameters
 MONGODB_URL = f"mongodb://{os.getenv('MONGOUSER')}:{os.getenv('MONGOPW')}{os.getenv('MONGOURL')}"
 MONGODB_DB = os.getenv('MONGODB')
-MONGODB_COLLECTION = 'stories'
+MONGODB_STORY_COLLECTION = 'stories'
+MONGODB_COMMENT_COLLECTION = 'comments'
 
 # Connect to MongoDB
 client = MongoClient(MONGODB_URL)
 db = client[MONGODB_DB]
-collection = db[MONGODB_COLLECTION]
+story_collection = db[MONGODB_STORY_COLLECTION]
+comment_collection = db[MONGODB_COMMENT_COLLECTION]
 
 # Connect to RabbitMQ
 connection = pika.BlockingConnection(pika.URLParameters(RABBITMQ_URL))
 channel = connection.channel()
 
-# Declare the queue
-channel.queue_declare(queue=RABBITMQ_QUEUE, durable=True)
+# Declare the queues
+channel.queue_declare(queue=RABBITMQ_STORIES_QUEUE, durable=True)
+channel.queue_declare(queue=RABBITMQ_COMMENTS_QUEUE, durable=True)
+
 
 # Define the callback function
-def callback(ch, method, properties, body):
+def story_callback(ch, method, properties, body):
     print("Received:", body)
 
     # Insert the message into MongoDB
     story = json.loads(body)
-    collection.insert_one(story)
+    story_collection.insert_one(story)
     print("Story inserted into MongoDB")
 
     # Acknowledge the message
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
+
+# Define the callback function for comments
+def comment_callback(ch, method, properties, body):
+    print("Received comment:", body)
+    comment = json.loads(body)
+    comment_collection.insert_one(comment)
+    print("Comment inserted into MongoDB")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
 # Start consuming messages
-channel.basic_consume(queue=RABBITMQ_QUEUE, on_message_callback=callback)
+channel.basic_consume(queue=RABBITMQ_STORIES_QUEUE, on_message_callback=story_callback)
+channel.basic_consume(queue=RABBITMQ_COMMENTS_QUEUE, on_message_callback=comment_callback)
 print('Waiting for new stories...')
 channel.start_consuming()
