e0c85ba4c68c5e6e01559d1e4b1f422bc051edfd
Morten Bendeke <mbendeke@hotmail.com>
2024-04-24 14:37:41 +0200

    added update story queue

--- subscriber.py
+++ subscriber.py
@@ -10,7 +10,8 @@ load_dotenv()
 print(os.getenv('RABBITUSER'))
 # RabbitMQ connection parameters
 RABBITMQ_URL = f"amqp://{os.getenv('RABBITUSER')}:{os.getenv('RABBITPW')}@{os.getenv('RABBITURL')}/%2F"
-RABBITMQ_STORIES_QUEUE = 'new_stories'
+RABBITMQ_NEW_STORIES_QUEUE = 'new_stories'
+RABBITMQ_UPDATE_STORY_QUEUE = 'update_story_info'
 RABBITMQ_COMMENTS_QUEUE = 'new_comments'
 
 # MongoDB connection parameters
@@ -30,8 +31,9 @@ connection = pika.BlockingConnection(pika.URLParameters(RABBITMQ_URL))
 channel = connection.channel()
 
 # Declare the queues
-channel.queue_declare(queue=RABBITMQ_STORIES_QUEUE, durable=True)
+channel.queue_declare(queue=RABBITMQ_NEW_STORIES_QUEUE, durable=True)
 channel.queue_declare(queue=RABBITMQ_COMMENTS_QUEUE, durable=True)
+channel.queue_declare(queue=RABBITMQ_UPDATE_STORY_QUEUE, durable=True)
 
 
 # Define the callback function
@@ -42,6 +44,21 @@ def story_callback(ch, method, properties, body):
     story = json.loads(body)
     story_collection.insert_one(story)
     print("Story inserted into MongoDB")
+
+    # Acknowledge the message
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+# Define the callback function for updating story info
+def update_story_info_callback(ch, method, properties, body):
+    print("Received story update:", body)
+    story_update = json.loads(body)
+
+    # Update the story in MongoDB
+    story_collection.update_one(
+        {'storyGuid': story_update['storyGuid']}, 
+        {'$set': {'storyInfo': story_update['storyInfo']}}
+    )
+    print("Story info updated in MongoDB")
 
     # Acknowledge the message
     ch.basic_ack(delivery_tag=method.delivery_tag)
@@ -57,7 +74,8 @@ def comment_callback(ch, method, properties, body):
 
 
 # Start consuming messages
-channel.basic_consume(queue=RABBITMQ_STORIES_QUEUE, on_message_callback=story_callback)
+channel.basic_consume(queue=RABBITMQ_NEW_STORIES_QUEUE, on_message_callback=story_callback)
 channel.basic_consume(queue=RABBITMQ_COMMENTS_QUEUE, on_message_callback=comment_callback)
-print('Waiting for new stories...')
+channel.basic_consume(queue=RABBITMQ_UPDATE_STORY_QUEUE, on_message_callback=update_story_info_callback)
+print('Waiting for new published content...')
 channel.start_consuming()
