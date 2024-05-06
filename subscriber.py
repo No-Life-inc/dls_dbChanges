d083f6154584ddecc56672b2455b3179eab749c9
Betül Iskender <betul.iskender@outlook.dk>
2024-05-06 13:30:15 +0200

    refactored so create comment works and made delete comment

--- subscriber.py
+++ subscriber.py
@@ -12,8 +12,10 @@ print(os.getenv('RABBITUSER'))
 RABBITMQ_URL = f"amqp://{os.getenv('RABBITUSER')}:{os.getenv('RABBITPW')}@{os.getenv('RABBITURL')}/%2F"
 RABBITMQ_NEW_STORIES_QUEUE = 'new_stories'
 RABBITMQ_UPDATE_STORY_QUEUE = 'update_story_info'
-RABBITMQ_COMMENTS_QUEUE = 'new_comments'
+RABBITMQ_NEW_COMMENTS_QUEUE = 'new_comments'
 RABBITMQ_DELETE_QUEUE = 'delete_story'
+RABBITMQ_DELETE_COMMENT_QUEUE = 'delete_comment'
+
 
 # MongoDB connection parameters
 MONGODB_URL = f"mongodb://{os.getenv('MONGOUSER')}:{os.getenv('MONGOPW')}{os.getenv('MONGOURL')}"
@@ -33,9 +35,10 @@ channel = connection.channel()
 
 # Declare the queues
 channel.queue_declare(queue=RABBITMQ_NEW_STORIES_QUEUE, durable=True)
-channel.queue_declare(queue=RABBITMQ_COMMENTS_QUEUE, durable=True)
+channel.queue_declare(queue=RABBITMQ_NEW_COMMENTS_QUEUE, durable=True)
 channel.queue_declare(queue=RABBITMQ_UPDATE_STORY_QUEUE, durable=True)
 channel.queue_declare(queue=RABBITMQ_DELETE_QUEUE, durable=True)
+channel.queue_declare(queue=RABBITMQ_DELETE_COMMENT_QUEUE, durable=True)
 
 # Define the callback function
 def story_callback(ch, method, properties, body):
@@ -71,16 +74,26 @@ def comment_callback(ch, method, properties, body):
     print("Received comment:", body)
     comment = json.loads(body)
 
+    # Extract the first commentInfo object from the array
+    comment_info = comment.get('commentInfos')[0] if comment.get('commentInfos') else {}
+
+    # Log the userGuid
+    user_guid = comment.get('user', {}).get('userGuid')
+    print(f"User GUID from comment: {user_guid}")
+
     story_collection.update_one(
-    {'storyGuid': comment.get('storyGuid')},
-    {'$push': {'comments': {
-        'commentGuid': comment.get('commentGuid'),
-        'createdAt': comment.get('createdAt'),
-        'commentInfos': comment.get('commentInfos'),
-        'user': comment.get('user')
-    }}}
-)
-    print("Story comment updated in MongoDB")
+        {'storyGuid': comment.get('story', {}).get('storyGuid')},
+        {'$push': {'comments': {
+            'commentGuid': comment.get('commentGuid'),
+            'createdAt': comment.get('createdAt'),
+            'commentInfo': {
+                'bodyText': comment_info.get('bodyText'),
+                'createdAt': comment_info.get('createdAt')
+            },
+            'user': comment.get('user')
+        }}}
+    )
+    print("Story updated with comment in MongoDB")
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 def delete_story_callback(ch, method, properties, body):
@@ -94,11 +107,29 @@ def delete_story_callback(ch, method, properties, body):
     # Acknowledge the message
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
+# Define the callback function for deleting comments
+def delete_comment_callback(ch, method, properties, body):
+    print("Received delete comment request:", body)
+    comment_guid = body.decode().strip('"') # decode the byte string to a normal string
+
+    # Delete the comment from MongoDB
+    result = story_collection.update_one(
+            {'comments.commentGuid': comment_guid}, 
+            {'$pull': {'comments': {'commentGuid': comment_guid}}}
+        )   
+    print(f"Comment {comment_guid} deleted from MongoDB, updated count: {result.modified_count}")
+
+
+    # Acknowledge the message
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
 
 # Start consuming messages
 channel.basic_consume(queue=RABBITMQ_NEW_STORIES_QUEUE, on_message_callback=story_callback)
-channel.basic_consume(queue=RABBITMQ_COMMENTS_QUEUE, on_message_callback=comment_callback)
+channel.basic_consume(queue=RABBITMQ_NEW_COMMENTS_QUEUE, on_message_callback=comment_callback)
 channel.basic_consume(queue=RABBITMQ_UPDATE_STORY_QUEUE, on_message_callback=update_story_info_callback)
 channel.basic_consume(queue=RABBITMQ_DELETE_QUEUE, on_message_callback=delete_story_callback)
+channel.basic_consume(queue=RABBITMQ_DELETE_COMMENT_QUEUE, on_message_callback=delete_comment_callback)
 print('Waiting for new published content...')
 channel.start_consuming()
